ad839278d5473aa7210489822f295cb9fe8765c7
Croustys <akos.barabas2002@gmail.com>
2022-09-18 14:29:39 +0200

    node vercel db impl

--- vercel/api/node/index.ts
+++ vercel/api/node/index.ts
@@ -1,3 +1,24 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export default function handler(req, res) {
-  return res.status(200).send("Vercel Node Serveless Performance test");
+  const connectionString = process.env.PG_URL;
+  const pool = new Pool({
+    connectionString,
+  });
+  const { name } = req.body;
+  const query = {
+    text: "INSERT INTO test_db (name) VALUES ($1)",
+    values: [name],
+  };
+  pool.query(query, (err, r) => {
+    if (err) {
+      console.log(err.stack);
+      return res.status(500).send("Unsuccesful database write");
+    }
+    pool.end();
+    return res.status(200).send("Successful creation");
+  });
 }

49c00e527d8e5482941c4507ebc09b46217aa284
Croustys <akos.barabas2002@gmail.com>
2022-09-18 14:29:51 +0200

    go vercel db impl

--- go.mod
+++ go.mod
@@ -3,3 +3,7 @@ module serverless-functions
 go 1.18
 
 require github.com/aws/aws-lambda-go v1.34.1
+
+require github.com/lib/pq v1.10.7
+
+require github.com/joho/godotenv v1.4.0
--- vercel/api/go/index.go
+++ vercel/api/go/index.go
@@ -1,10 +1,68 @@
 package handler
 
 import (
+	"database/sql"
+	"encoding/json"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+func connectToDb() *sql.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("PG_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Vercel Go Serveless Performance test"))
+	switch method := r.Method; method {
+	case "POST":
+		if create(r) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("Successful creation"))
+			return
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Unsuccesful database write"))
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Unsupported Method"))
+	}
+}
+
+type reqBody struct {
+	Name string `json:"name"`
+}
+
+func create(r *http.Request) bool {
+	db := connectToDb()
+	defer db.Close()
+
+	var rq reqBody
+	err := json.NewDecoder(r.Body).Decode(&rq)
+	if err != nil {
+		log.Println(err)
+	}
+
+	result, err := db.Exec("INSERT INTO test_db (name) VALUES ($1)", rq.Name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(rows)
+
+	return rows == 1
 }
